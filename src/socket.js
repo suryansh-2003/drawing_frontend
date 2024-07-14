8f1f85e827b3676f844726a2cc014ff6abdf50f9
Suryansh Choudhary <suryansh.choudhary999@gmail.com>
2024-07-14 17:13:26 +0530

    deployment

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5001'
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketch-ease-server.onrender.com' : 'http://localhost:5001'
 export const socket = io(URL);
